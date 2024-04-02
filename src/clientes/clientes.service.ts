22e09f9555f1968b5b273dacca38431933494819
AlejandroGuillenB <guillen4024@gmail.com>
2024-04-02 09:37:57 -0500

    add eslin

--- src/clientes/clientes.service.ts
+++ src/clientes/clientes.service.ts
@@ -1,18 +1,18 @@
-import { Injectable } from '@nestjs/common';
-import { ClientesRepository } from './cliente.repository';
-import { ClienteMapper } from './cliente.mapper';
-import { ClienteDTO } from './cliente.dto';
-import { ClienteEntity } from './cliente.entity';
+import { Injectable } from '@nestjs/common'
+import { ClientesRepository } from './cliente.repository'
+import { ClienteMapper } from './cliente.mapper'
+import { type ClienteDTO } from './cliente.dto'
+import { type ClienteEntity } from './cliente.entity'
 
 @Injectable()
 export class ClienteService {
-    constructor(
-        private clientesRepository: ClientesRepository,
-        private mapper: ClienteMapper,
-    ) {}
+  constructor (
+    private readonly clientesRepository: ClientesRepository,
+    private readonly mapper: ClienteMapper
+  ) {}
 
-    async getAllClientes(): Promise<ClienteDTO[]> {
-        const clientes: ClienteEntity[] = await this.clientesRepository.getAllClientes();
-        return clientes.map((cliente) => this.mapper.entityToDto(cliente));
-    }
+  async getAllClientes (): Promise<ClienteDTO[]> {
+    const clientes: ClienteEntity[] = await this.clientesRepository.getAllClientes()
+    return clientes.map((cliente) => this.mapper.entityToDto(cliente))
+  }
 }
