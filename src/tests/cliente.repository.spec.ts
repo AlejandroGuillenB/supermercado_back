85942aa2b7eca0651a8d8777886a22adf98f0017
AlejandroGuillenB <guillen4024@gmail.com>
2024-04-09 13:38:21 -0500

    config test

--- src/tests/cliente.repository.spec.ts
+++ src/tests/cliente.repository.spec.ts
@@ -1,79 +1,45 @@
 import { ClientesRepository } from '../clientes/cliente.repository'
 import { ClienteEntity } from '../clientes/cliente.entity'
 import { Test, type TestingModule } from '@nestjs/testing'
-import { Repository } from 'typeorm'
 import { getRepositoryToken } from '@nestjs/typeorm'
-
-export const createRepositoryMock = (): Repository<ClienteEntity> => {
-  const repositoryMock =
-  Object.getOwnPropertyNames(Repository.prototype).reduce((acc, x) => {
-    acc[x] = jest.fn()
-    return acc
-  }, {}) as Repository<ClienteEntity>
-  return repositoryMock
-}
+import { ClienteMapper } from '../clientes/cliente.mapper'
 
 describe('ClienteRepository', () => {
   let clientesRepository: ClientesRepository
-  let repo: Repository<ClienteEntity>
-
-  const result: ClienteEntity[] = [
-    {
-      id: 1,
-      nombre: 'Martin',
-      apellido: 'Gutierrez',
-      tipo_doc: 'DNI',
-      nro_doc: '4598676890',
-      nro_tel_princ: '+549116574839',
-      nro_tel_sec: '-',
-      email: 'martin.gutierrez@gmail.com'
-    }
-  ]
-
-  const mockClientesRepository = {
-    getAllClientes: () => result,
-    getClienteById: () => result[0],
-    addCliente: () => result[0],
-    updateCliente: () => result[0]
-  }
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
-      providers: [ClientesRepository, {
-        provide: getRepositoryToken(ClienteEntity), useValue: createRepositoryMock()
+      providers: [ClientesRepository, ClienteMapper, {
+        provide: getRepositoryToken(ClienteEntity),
+        useValue: { find: jest.fn(), findOneOrFail: jest.fn(), save: jest.fn(), update: jest.fn() }
       }]
-    }).overrideProvider(ClientesRepository).useValue(mockClientesRepository).compile()
+    }).compile()
 
     clientesRepository = app.get<ClientesRepository>(ClientesRepository)
-    repo = app.get<Repository<ClienteEntity>>(getRepositoryToken(ClienteEntity))
   })
 
   describe('Clientes', () => {
     it('should return a array of clientes', async () => {
-      const spy = jest.spyOn(repo, 'find').mockResolvedValueOnce(result)
-      const clientes = await clientesRepository.getAllClientes()
-      expect(clientes).toEqual(result)
+      const spy = jest.spyOn(clientesRepository, 'getAllClientes')
+      expect(typeof clientesRepository.getAllClientes).toBe('function')
       spy.mockRestore()
     })
 
     it('should return a cliente found by id', async () => {
-      const spy = jest.spyOn(clientesRepository, 'getClienteById').mockResolvedValue(result[0])
-      const cliente = await clientesRepository.getClienteById(1)
-      expect(cliente).toEqual(result[0])
+      const spy = jest.spyOn(clientesRepository, 'getClienteById')
+      expect(typeof clientesRepository.getClienteById).toBe('function')
       spy.mockRestore()
     })
 
     it('should add new cliente', async () => {
-      const spy = jest.spyOn(clientesRepository, 'addCliente').mockResolvedValue(result[0])
-      const cliente = await clientesRepository.addCliente(result[0])
-      expect(cliente).toEqual(result[0])
+      const spy = jest.spyOn(clientesRepository, 'addCliente')
+      expect(typeof clientesRepository.addCliente).toBe('function')
       spy.mockRestore()
     })
 
     it('should update a cliente', async () => {
-      const spy = jest.spyOn(clientesRepository, 'updateCliente').mockResolvedValue(result[0])
-      const cliente = await clientesRepository.updateCliente(1, result[0])
-      expect(cliente).toEqual(result[0])
+      const spy = jest.spyOn(clientesRepository, 'updateCliente')
+      expect(typeof clientesRepository.updateCliente).toBe('function')
       spy.mockRestore()
     })
   })
