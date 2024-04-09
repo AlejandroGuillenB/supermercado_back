790c3818787730d0572a0be2ca1cfb09e1a2d842
AlejandroGuillenB <guillen4024@gmail.com>
2024-04-09 18:40:49 -0500

    add test

--- src/tests/auth.service.spec.ts
+++ src/tests/auth.service.spec.ts
@@ -5,18 +5,37 @@ import { JwtService } from '@nestjs/jwt';
 
 describe('AuthService', () => {
   let service: AuthService;
+  const users = [
+    {
+      userId: 1,
+      username: 'john',
+      password: 'changeme',
+    },
+    {
+      userId: 2,
+      username: 'maria',
+      password: 'guess',
+    },
+  ];
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
+    const app: TestingModule = await Test.createTestingModule({
       providers: [AuthService, UsersService, JwtService],
     }).compile();
 
-    service = module.get<AuthService>(AuthService);
+    service = app.get<AuthService>(AuthService);
   });
 
   describe('Auth', () => {
     it('should be defined', () => {
       expect(service).toBeDefined();
+    });
+
+    it('should be sign In', async () => {
+      const spy = jest.spyOn(service, 'signIn').mockResolvedValue({ access_token: 'token' })
+      const user = await service.signIn(users[0].username, users[0].password);
+      expect(user).toEqual({ access_token: 'token' })
+      spy.mockRestore();
     });
   });
 });
