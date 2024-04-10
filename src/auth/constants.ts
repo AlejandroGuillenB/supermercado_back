1b67558a1010d3b790794186523fe7fe3d8eff5d
AlejandroGuillenB <guillen4024@gmail.com>
2024-04-09 19:46:36 -0500

    config constats

--- src/auth/constants.ts
+++ src/auth/constants.ts
@@ -1,3 +1,16 @@
 export const jwtConstants = {
   secret: 'Lfb2AVFmkVNOzSslUBbaM6p7U4MMUK5u',
 }
+
+export const users = [
+  {
+    userId: 1,
+    username: 'john',
+    password: 'changeme',
+  },
+  {
+    userId: 2,
+    username: 'maria',
+    password: 'guess',
+  },
+];
