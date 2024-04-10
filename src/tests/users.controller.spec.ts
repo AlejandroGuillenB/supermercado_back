9934f956cf6e208e03ea21d260436546fb76619b
AlejandroGuillenB <guillen4024@gmail.com>
2024-04-10 16:35:59 -0500

    add test

---
+++ src/tests/users.controller.spec.ts
@@ -0,0 +1,71 @@
+import { type TestingModule, Test } from '@nestjs/testing';
+import { UsersController } from '../users/users.controller'
+import { type UsersDTO } from '../users/users.dto'
+import { UsersService } from '../users/users.service';
+
+describe('UsersController', () => {
+  let usersController: UsersController;
+  let usersService: UsersService;
+  const users: UsersDTO[] = [
+    {
+      id: 1,
+      username: 'Juan',
+      password: '123'
+    },
+    {
+      id: 2,
+      username: 'Pedro',
+      password: '123'
+    }
+  ]
+  const mockUsersService = {
+    getAllUsers: () => users,
+    getUserById: () => users[0],
+    newUser: () => users[0],
+    updateUser: () => users[0],
+    deleteUser: () => { }
+  }
+
+  beforeEach(async () => {
+    const app: TestingModule = await Test.createTestingModule({
+      controllers: [UsersController],
+      providers: [UsersService]
+    }).overrideProvider(UsersService).useValue(mockUsersService).compile();
+
+    usersController = app.get<UsersController>(UsersController);
+    usersService = app.get<UsersService>(UsersService);
+  });
+
+  describe('Users', () => {
+    it('should return an array of users', async () => {
+      const spy = jest.spyOn(usersService, 'getAllUsers').mockImplementation(async () => users)
+      expect(await usersController.getAllUsers()).toBe(users)
+      spy.mockRestore()
+    });
+
+    it('should return a user found by id', async () => {
+      const spy = jest.spyOn(usersService, 'getUserById').mockImplementation(async () => users[0])
+      expect(await usersController.getUserById(1)).toBe(users[0])
+      spy.mockRestore()
+    });
+
+    it('should add new user', async () => {
+      const spy = jest.spyOn(usersService, 'newUser').mockImplementation(async () => users[0])
+      expect(await usersController.newUser(users[0])).toBe(users[0])
+      spy.mockRestore()
+    });
+
+    it('should update a user', async () => {
+      const spy = jest.spyOn(usersService, 'updateUser').mockImplementation(async () => users[0])
+      expect(await usersController.updateUser(1, users[0])).toBe(users[0])
+      spy.mockRestore()
+    });
+
+    it('should delete a user', async () => {
+      const spy = jest.spyOn(usersService, 'deleteUser').mockImplementation()
+      // eslint-disable-next-line @typescript-eslint/no-confusing-void-expression
+      expect(await usersController.deleteUser(1)).toBe(undefined)
+      spy.mockRestore()
+    });
+  });
+});
--- src/tests/users.service.spec.ts
+++ src/tests/users.service.spec.ts
@@ -5,7 +5,8 @@ import { UsersMapper } from '../users/users.mapper';
 import { type UsersEntity } from '../users/users.entity';
 
 describe('UsersService', () => {
-  let service: UsersService;
+  let usersService: UsersService;
+  let usersRepository: UsersRepository;
 
   const users: UsersEntity[] = [
     {
@@ -31,13 +32,20 @@ describe('UsersService', () => {
       }
     },
   ];
+
+  const newUser = {
+    id: 1,
+    username: 'john',
+    password: 'changeme'
+  };
+
   const mockUsersRepository = {
     getAllUsers: () => users,
     getUserById: () => users[0],
     getUserByName: () => users[0],
     newUser: () => users[0],
     updateUser: () => users[0],
-    deleteUser: () => {}
+    deleteUser: () => { }
   }
 
   beforeEach(async () => {
@@ -45,12 +53,50 @@ describe('UsersService', () => {
       providers: [UsersService, UsersRepository, UsersMapper],
     }).overrideProvider(UsersRepository).useValue(mockUsersRepository).compile();
 
-    service = app.get<UsersService>(UsersService);
+    usersService = app.get<UsersService>(UsersService);
+    usersRepository = app.get<UsersRepository>(UsersRepository);
   });
 
   describe('Users', () => {
     it('should be defined', () => {
-      expect(service).toBeDefined();
+      expect(usersService).toBeDefined();
+    });
+
+    it('should return a array of users', async () => {
+      const spy = jest.spyOn(usersRepository, 'getAllUsers').mockImplementation(async () => users);
+      expect((await usersService.getAllUsers()).length).toEqual(2);
+      spy.mockRestore();
+    });
+
+    it('should found a user by id', async () => {
+      const spy = jest.spyOn(usersRepository, 'getUserById').mockImplementation(async () => users[0]);
+      expect((await usersService.getUserById(1)).id).toEqual(users[0].userid);
+      spy.mockRestore();
+    });
+
+    it('should found a user by username', async () => {
+      const spy = jest.spyOn(usersRepository, 'getUserByName').mockImplementation(async () => users[0]);
+      expect((await usersService.getUserByName(users[0].username)).username).toEqual(users[0].username);
+      spy.mockRestore();
+    });
+
+    it('should add a new user', async () => {
+      const spy = jest.spyOn(usersRepository, 'newUser').mockImplementation(async () => users[0]);
+      expect((await usersService.newUser(newUser)).username).toEqual(users[0].username);
+      spy.mockRestore();
+    });
+
+    it('should update a user', async () => {
+      const spy = jest.spyOn(usersRepository, 'updateUser').mockImplementation(async () => users[0]);
+      expect((await usersService.updateUser(1, newUser)).username).toEqual(users[0].username);
+      spy.mockRestore();
+    });
+
+    it('should delete a user', async () => {
+      const spy = jest.spyOn(usersRepository, 'deleteUser').mockImplementation();
+      // eslint-disable-next-line @typescript-eslint/no-confusing-void-expression
+      expect(await usersService.deleteUser(1)).toBe(undefined);
+      spy.mockRestore();
     });
   });
 });
