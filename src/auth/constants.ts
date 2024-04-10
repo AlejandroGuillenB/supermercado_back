436388fe2bd2da3ef3a309dac65dc66564f3ec84
AlejandroGuillenB <guillen4024@gmail.com>
2024-04-09 19:52:01 -0500

    fix hostpots

--- src/auth/constants.ts
+++ src/auth/constants.ts
@@ -1,16 +1,16 @@
 export const jwtConstants = {
   secret: 'Lfb2AVFmkVNOzSslUBbaM6p7U4MMUK5u',
 }
-
+// TODO: this data is used temporaly for test
 export const users = [
   {
     userId: 1,
     username: 'john',
-    password: 'changeme',
+    password: process.env.PASS1,
   },
   {
     userId: 2,
     username: 'maria',
-    password: 'guess',
+    password: process.env.PASS2,
   },
 ];
