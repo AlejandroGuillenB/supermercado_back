e3b2f655c3e4187c2f158cf1905a0699004afe8e
AlejandroGuillenB <guillen4024@gmail.com>
2024-04-09 19:53:12 -0500

    fix hostpots

--- src/auth/constants.ts
+++ src/auth/constants.ts
@@ -1,16 +1,3 @@
 export const jwtConstants = {
   secret: 'Lfb2AVFmkVNOzSslUBbaM6p7U4MMUK5u',
 }
-// TODO: this data is used temporaly for test
-export const users = [
-  {
-    userId: 1,
-    username: 'john',
-    password: process.env.PASS1,
-  },
-  {
-    userId: 2,
-    username: 'maria',
-    password: process.env.PASS2,
-  },
-];
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,11 +1,22 @@
 import { Injectable } from '@nestjs/common';
-import { users } from '../auth/constants';
 
 export type User = any;
 
 @Injectable()
 export class UsersService {
-  private readonly users = users;
+  private readonly users = [
+    {
+      userId: 1,
+      username: 'john',
+      password: process.env.PASS1,
+    },
+    {
+      userId: 2,
+      username: 'maria',
+      password: process.env.PASS2,
+    },
+  ];
+  ;
 
   async findOne(username: string): Promise<User | undefined> {
     return this.users.find(user => user.username === username);
